e846f680837bad7b872225eaa6270144a3420ba2
jnanesw <nanimca000@gmail.com>
2024-01-31 20:26:11 +0530

    Commited

--- src/WebsiteBuilder.js
+++ src/WebsiteBuilder.js
@@ -93,7 +93,7 @@ const WebsiteBuilder = () => {
                 onChange={(e) => handleSectionContentChange(section.id, e.target.value)}
               />
             ) : (
-              <img src={section.content} alt="image" width="100" height="100" />
+              <img src={section.content} alt="dynamic Uploads" width="100" height="100" />
             )}
             <button onClick={() => handleSectionDelete(section.id)}>Delete</button>
           </div>
